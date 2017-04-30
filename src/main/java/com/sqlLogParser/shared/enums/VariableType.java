4a9403b22d13de86fe5e1e6b02e8ebc988a6e684
Jakub Dużyk <jakub.duzyk@gmail.com>
2017-04-30 22:34:24 +0200

    VariableType enum removed

--- src/main/java/com/sqlLogParser/shared/enums/VariableType.java
+++
@@ -1,9 +0,0 @@
-package com.sqlLogParser.shared.enums;
-
-/**
- * Created by Jakub on 30.04.2017.
- */
-public enum VariableType
-{
-    STRING, OTHER
-}
--- src/test/java/com/sqlLogParser/shared/logs/LogParserTest.java
+++ src/test/java/com/sqlLogParser/shared/logs/LogParserTest.java
@@ -37,7 +37,7 @@ public class LogParserTest
 
         Assert.assertEquals(expected, actual);
     }
-    
+
     @Test
     public void getParamValuesFrom() throws Exception
     {
